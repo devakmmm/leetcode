e7ed83555fd9eec932e93b4c4e48a61e6b8c6bdb
devakmmm <mdevak44@gmail.com>
2026-01-07 17:22:13 -0500

    leetcode practise for arrays and hashing added

--- arrays and hashing/group_anagrams.py
+++ arrays and hashing/group_anagrams.py
@@ -5,6 +5,8 @@ class Solution:
         for s in strs:
             key=''.join(sorted(s)) #what this line does is adds each string after sorting it to the key example: "eat" becomes "aet"
             groups[key].append(s) #this line appends the original string to the list of anagrams corresponding to the sorted key example: "eat" is appended to the list for key "aet"
+
+            ## groups["".join(sorted(s))].append(s) #this line does the same as above in a single line so basically we are addeing sorted words as key and orignal words that are anagrams ie that mean the same as key to the list of that key as values
         
         return list(groups.values())
     # the above program groups anagrams together from a list of strings
--- runnerup_hackathon.py
+++ runnerup_hackathon.py
@@ -1,8 +1,8 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr=list(map(int, input().split()))
+if __name__ == '__main__':  # Check if script is run directly
+    n = int(input())  # Read number of elements
+    arr = list(map(int, input().split()))  # Read list of integers
     
-    maxscore=max(arr)
-    filteredlist=[score for score in arr if score is not maxscore]
-    runnerscore=max(filteredlist)
-    print(runnerscore)
+    maxscore = max(arr)  # Find the maximum value
+    filteredlist = [score for score in arr if score is not maxscore]  # List of values not the same object as maxscore (should use != for value)
+    runnerscore = max(filteredlist)  # Find the runner-up (second highest) value
+    print(runnerscore)  # Print the runner-up score
